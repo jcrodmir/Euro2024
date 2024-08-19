1600e5e3e1b82f1a846be828db4afd696cd2a7c2
jcrodmir <jcrmtrabajo@gmail.com>
2024-08-19 20:24:48 +0200

    Primer commit

---
+++ src/main/java/com/Euro2024/Eurocopa2024Application.java
@@ -0,0 +1,13 @@
+package com.Euro2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eurocopa2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eurocopa2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Euro2024/Eurocopa2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Euro2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eurocopa2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
