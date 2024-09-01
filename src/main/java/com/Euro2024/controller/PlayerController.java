c03bf5284785bb261b04808ff67e02c6ff7e6719
jcrodmir <jcrmtrabajo@gmail.com>
2024-09-01 23:22:25 +0200

    feat:Bucket4J for too many requests

--- src/main/java/com/Euro2024/controller/PlayerController.java
+++ src/main/java/com/Euro2024/controller/PlayerController.java
@@ -4,146 +4,207 @@ import com.Euro2024.dto.PlayerDto;
 import com.Euro2024.dto.GenericResponse;
 import com.Euro2024.models.PlayerEntity;
 import com.Euro2024.service.PlayerService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/")//Structure to call requests.
 public class PlayerController {
     private PlayerService playerService;
+    private final Bucket bucket;
+    private final int tokens=10;
+    private final int capacity=10;
+
+
+
     @Autowired
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
+        Bandwidth limit=Bandwidth.classic(capacity, Refill.greedy(tokens, Duration.ofMinutes(1)));
+
+        this.bucket = Bucket.builder().addLimit(limit).build();
     }
 
     @GetMapping("player")
     public ResponseEntity<GenericResponse<PlayerDto>> getPlayers(@RequestParam(value = "pageNo", defaultValue = "0",required = false) int pageNo,
                                                       @RequestParam(value = "pageSize", defaultValue = "10",required = false) int pageSize) {
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+
 
         return new ResponseEntity<GenericResponse<PlayerDto>>(playerService.getAllPlayer(pageNo,pageSize),HttpStatus.OK);
     }
 
     @GetMapping("player/id/{id}")
     public ResponseEntity<PlayerDto> playerDetail(@PathVariable int id){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+
         return ResponseEntity.ok(playerService.getPlayerById(id));
 
     }
     @GetMapping("player/name")
     public ResponseEntity<PlayerDto> playerName(@RequestParam(value = "name", defaultValue = "unknown",required = false) String name){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+
         return ResponseEntity.ok(playerService.getPlayerByName(name));
 
     }
     @GetMapping("player/goal")
     public ResponseEntity<List<PlayerDto>> playerGoal(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberByGoals(number));
 
     }
     @GetMapping("player/goals")
     public ResponseEntity<List<PlayerDto>> playerGoals(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByGoals());
 
     }
     @GetMapping("player/yellowcard")
     public ResponseEntity<List<PlayerDto>> playerYellow(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberYellowCards(number));
 
     }
     @GetMapping("player/yellowcards")
     public ResponseEntity<List<PlayerDto>> playerYellowCard(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByYellowCard());
 
     }
 
     @GetMapping("player/redcard")
     public ResponseEntity<List<PlayerDto>> playerRed(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberRedCards(number));
 
     }
     @GetMapping("player/redcards")
     public ResponseEntity<List<PlayerDto>> playerRedCard(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByRedCard());
 
     }
     @GetMapping("player/save")
     public ResponseEntity<List<PlayerDto>> playerGoalKeeper(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberGoalKeeperSaves(number));
 
     }
     @GetMapping("player/saves")
     public ResponseEntity<List<PlayerDto>> playerGoalKeepers(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByGoalKeeperSaves());
 
     }
     @GetMapping("player/assist")
     public ResponseEntity<List<PlayerDto>> playerAssist(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberAssists(number));
 
     }
     @GetMapping("player/assists")
     public ResponseEntity<List<PlayerDto>> playerAssists(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByAssists());
 
     }
     @GetMapping("player/recover")
     public ResponseEntity<List<PlayerDto>> playerRecovered(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberRecovered(number));
 
     }
     @GetMapping("player/recovered")
     public ResponseEntity<List<PlayerDto>> playerrecovered(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByRecovered());
 
     }
     @GetMapping("player/dorsal")
     public ResponseEntity<List<PlayerDto>> playerDorsal(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberByDorsal(number));
 
     }
     @GetMapping("player/dorsals")
     public ResponseEntity<List<PlayerDto>> playerDorsals(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByDorsal());
 
     }
 
     @GetMapping("player/minute")
     public ResponseEntity<List<PlayerDto>> playerMinutes(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberMinutes(number));
 
     }
     @GetMapping("player/minutes")
     public ResponseEntity<List<PlayerDto>> playerMinutes(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByMinutes());
 
     }
     @GetMapping("player/match")
     public ResponseEntity<List<PlayerDto>> playerMatch(@RequestParam(value = "number", defaultValue = "0" ,required = false) int number){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerNumberMatches(number));
 
     }
     @GetMapping("player/matches")
     public ResponseEntity<List<PlayerDto>> playerMatches(){
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByMatches());
 
     }
@@ -153,6 +214,9 @@ public class PlayerController {
         if(player.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No players found for the given position");
         }
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getPlayerByPosition(position));
 
     }
@@ -160,6 +224,9 @@ public class PlayerController {
 
     @GetMapping("player/team/{teamId}")
     public ResponseEntity<List<PlayerDto>> teamPlayersDetail(@PathVariable int teamId){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(playerService.getAllPlayersFromTeam(teamId));
 
     }
--- src/main/java/com/Euro2024/controller/TeamController.java
+++ src/main/java/com/Euro2024/controller/TeamController.java
@@ -5,11 +5,15 @@ import com.Euro2024.dto.PlayerDto;
 import com.Euro2024.dto.TeamDto;
 import com.Euro2024.models.PlayerEntity;
 import com.Euro2024.service.TeamService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.List;
 
 @RestController
@@ -17,20 +21,33 @@ import java.util.List;
 public class TeamController {
 
     private TeamService teamService;
+    private final Bucket bucket;
+    private final int tokens=10;
+    private final int capacity=10;
+
     @Autowired
     public TeamController(TeamService teamService) {
+
         this.teamService = teamService;
+        Bandwidth limit=Bandwidth.classic(capacity, Refill.greedy(tokens, Duration.ofMinutes(1)));
+
+        this.bucket = Bucket.builder().addLimit(limit).build();
     }
 
     @GetMapping("team")
     public ResponseEntity<GenericResponse<TeamDto>> getTeams(@RequestParam(value = "pageNo", defaultValue = "0",required = false) int pageNo,
                                                                  @RequestParam(value = "pageSize", defaultValue = "10",required = false) int pageSize) {
-
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return new ResponseEntity<GenericResponse<TeamDto>>(teamService.getAllTeam(pageNo,pageSize), HttpStatus.OK);
     }
 
     @GetMapping("team/{id}")
     public ResponseEntity<TeamDto> teamDetail(@PathVariable int id){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getTeamById(id));
 
     }
@@ -39,49 +56,75 @@ public class TeamController {
     @PostMapping("team/create")
     @ResponseStatus(HttpStatus.CREATED)
     public  ResponseEntity<TeamDto> createTeam(@RequestBody TeamDto team){
-
         return new ResponseEntity<>(teamService.createTeam(team), HttpStatus.CREATED);
     }
     /////////////////////////////////////////////////////////////////
     @GetMapping("team/players")
     public ResponseEntity<List<PlayerDto>> teamOnlyPlayers(@RequestParam(value = "id", defaultValue = "0" ,required = false) int id){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getPlayersByTeamId(id));
 
     }
     @GetMapping("team/country")
     public ResponseEntity<TeamDto> getTeamByCountry(@RequestParam(value = "country", defaultValue = "" ,required = false) String country){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getByCountry(country));
 
     }
     @GetMapping("team/{country}/coach")
     public ResponseEntity<String> getCoachbyTeam(@PathVariable String country) {
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getCoachByCountry(country));
     }
     @GetMapping("team/{country}/federation")
     public ResponseEntity<String> getFederationbyTeam(@PathVariable String country) {
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getFederationByCountry(country));
     }
     @GetMapping("team/{country}/championships")
     public ResponseEntity<Integer> getChampionshipsbyTeam(@PathVariable String country) {
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getChampionshipsByCountry(country));
     }
 
     @GetMapping("team/coach")
     public ResponseEntity<TeamDto> getTeamByCoach(@RequestParam(value = "coach", defaultValue = "" ,required = false) String coach) {
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getByCoach(coach));
     }
     @GetMapping("team/federation")
     public ResponseEntity<TeamDto> getTeamByFederation(@RequestParam(value = "federation", defaultValue = "" ,required = false) String federation){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getByFederation(federation));
 
     }
     @GetMapping("team/championships")
     public ResponseEntity<TeamDto> getTeamByChampionships(@RequestParam(value = "championships", defaultValue = "0" ,required = false) int championships){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getByChampionships(championships));
 
     }
     @GetMapping("team/foundation")
     public ResponseEntity<TeamDto> getTeamByFoundation(@RequestParam(value = "foundation", defaultValue = "0" ,required = false) int foundation){
+        if (!bucket.tryConsume(tokens)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
         return ResponseEntity.ok(teamService.getByFoundation(foundation));
 
     }
