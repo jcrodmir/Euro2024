0a9a786dec1572b3fcb1b175567daa96f1925f13
jcrodmir <jcrmtrabajo@gmail.com>
2024-10-05 21:09:43 +0200

    fix:  scheduler

--- src/main/java/com/Euro2024/scheduler/SchedulerConfig.java
+++ src/main/java/com/Euro2024/scheduler/SchedulerConfig.java
@@ -2,8 +2,8 @@ package com.Euro2024.scheduler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-/*
+
 @Configuration
 @EnableScheduling
 public class SchedulerConfig {
-}*/
+}
